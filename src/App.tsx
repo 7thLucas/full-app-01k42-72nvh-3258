323268aa11c2592b9456a04533bd4030dfb38543
Nathanael <nael.nathanael71@gmail.com>
2025-06-11 13:14:01 +0700

    feat: return all template back as it should be

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { OmniForm } from "./components/OmniForm";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <OmniForm
-        fields={[
-          {
-            name: "name",
-            label: "Name",
-            type: "text",
-            required: true,
-          },
-          {
-            name: "photo_one",
-            label: "Photo One",
-            type: "file",
-            fileType: "image",
-            required: true,
-          },
-          {
-            name: "file_one",
-            label: "File One",
-            type: "file",
-            required: true,
-          },
-        ]}
-      />
-    </div>
+    <Routes>
+      <Route element={<div>Hello World</div>} path="/" />
+    </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import "@/globals.css";
 
@@ -7,9 +8,11 @@ import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HeroUIProvider>
-      <ToastProvider placement="top-right" toastOffset={10} />
-      <App />
-    </HeroUIProvider>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
+      <HeroUIProvider>
+        <ToastProvider placement="top-right" toastOffset={10} />
+        <App />
+      </HeroUIProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
