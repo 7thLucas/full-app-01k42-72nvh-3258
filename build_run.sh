1a94de89cb0b02cabfe04dc2ced5c4d0c0c07fac
Nathanael <nael.nathanael71@gmail.com>
2025-06-12 23:13:31 +0700

    htfx

--- build_run.sh
+++ build_run.sh
@@ -5,7 +5,7 @@ bun run build
 # Check if --local flag is passed
 if [[ "$1" != "--local" ]]; then
     sudo mkdir -p /var/www/qb-poc-preview
-    sudo rm -rf /var/www/qb-poc-preview/####VAR:FOLDER_ID####
-    sudo mv dist /var/www/qb-poc-preview/####VAR:FOLDER_ID####
+    sudo rm -rf /var/www/qb-poc-preview/01JGXM8F9QR2N3P4K5T6V7W8X9
+    sudo mv dist /var/www/qb-poc-preview/01JGXM8F9QR2N3P4K5T6V7W8X9
     sudo chown -R www-data:www-data /var/www/qb-poc-preview/
 fi
